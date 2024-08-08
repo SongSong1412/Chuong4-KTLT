5d8d8e72ccf68994e55281c9cafffdb2a449c0cb
SongSong1412 <ntaun073003@gmail.com>
2024-08-08 11:58:35 +0700

    Tim phan tu thu n cua cap so cong co so hang dau la a, cong saila r

---
+++ Chuong4_KTLT_P1/Chuong4_KTLT_P1/Bai2.cpp
@@ -0,0 +1,26 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//// Hàm đệ quy tính giá trị phần tử thứ n của cấp số cộng
+//int Un(int n, int a, int r) {
+//	if (n == 1)
+//		return a;
+//	return Un(n - 1, a, r) + r;
+//}
+//
+//int main() {
+//	int n, a, r;
+//	printf("Nhap gia tri cua n: ");
+//	scanf("%d", &n);
+//	printf("Nhap gia tri cua a (so hang dau): ");
+//	scanf("%d", &a);
+//	printf("Nhap gia tri cua r (cong sai): ");
+//	scanf("%d", &r);
+//
+//	int result = Un(n, a, r);
+//	printf("Gia tri phan tu thu %d cua cap so cong la: %d\n", n, result);
+//
+//	system("pause");
+//	return 0;
+//}
