815c8b0076cffe2d63b7beb0f23a42d28ceef551
Le Ngoc Dinh Tri <dinhtri5112003@gmail.com>
2024-08-08 20:17:02 +0700

    finish ketqua Chuong4_Bai10_viet ham tim kiem so x tren a theo thuat toan tim kiem nhi phan bang ky thuat de quy

---
+++ Chuong4_KTLT_P2/Bai10.cpp
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+
+// Ham tim kiem nhi phan de quy
+int TimKiemNhiPhan(int a[], int left, int right, int x) {
+	if (left <= right) {
+		int mid = left + (right - left) / 2;
+
+		// Neu phan tu o giua la x
+		if (a[mid] == x)
+			return mid;
+
+		// Neu x nho hon phan tu o giua, tìm kiếm bên trái
+		if (a[mid] > x)
+			return TimKiemNhiPhan(a, left, mid - 1, x);
+
+		// Neu x lon hon phan tu o giua, tim kiem ben phai
+		return TimKiemNhiPhan(a, mid + 1, right, x);
+	}
+
+	// Neu khong tim thay x
+	return -1;
+}
+
+int main() {
+	int a[] = { 1, 3, 5, 7, 9, 21, 33, 45, 57, 79 };
+	int n = sizeof(a) / sizeof(a[0]);
+	int x;
+
+	printf("Nhap gia tri can tim: ");
+	scanf("%d", &x);
+
+	int result = TimKiemNhiPhan(a, 0, n - 1, x);
+
+	if (result != -1)
+		printf("Gia tri %d duoc tim thay tai vi tri %d.\n", x, result);
+	else
+		printf("Gia tri %d khong co trong mang.\n", x);
+
+	return 0;
+}
