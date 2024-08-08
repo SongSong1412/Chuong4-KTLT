59a9c77266d71173827c2049ba801ac823a9d32e
SongSong1412 <ntaun073003@gmail.com>
2024-08-08 12:02:04 +0700

    Viet ham tim uoc chung lon nhat cua 2 so nguyen duong a va b

---
+++ Chuong4_KTLT_P1/Chuong4_KTLT_P1/Bai6.cpp
@@ -0,0 +1,27 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//// Hàm tìm ước chung lớn nhất
+//int gcd(int a, int b) {
+//	while (b != 0) {
+//		int temp = b;
+//		b = a % b;
+//		a = temp;
+//	}
+//	return a;
+//}
+//
+//int main() {
+//	int a, b;
+//	printf("Nhap a: ");
+//	scanf("%d", &a);
+//	printf("Nhap b: ");
+//	scanf("%d", &b);
+//
+//	int result = gcd(a, b);
+//	printf("UCLN cua %d va %d la: %d\n", a, b, result);
+//
+//	system("pause");
+//	return 0;
+//}
