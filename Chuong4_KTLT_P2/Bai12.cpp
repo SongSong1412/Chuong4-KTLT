4667dbe571a0c21e8921abfe016c58f5b4252807
Le Ngoc Dinh Tri <dinhtri5112003@gmail.com>
2024-08-08 20:59:32 +0700

    finish Chuong4_Bai12_Viet ham tim so Fibonacci lon nhat nhung nho hon so nguyen n cho truoc theo cach khu de quy

---
+++ Chuong4_KTLT_P2/Bai12.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+// Ham tim so Fibonacci lon nhat nhưng nho hon n (khu de quy)
+long long FibonaccimaxKhuDeQuy(int n) {
+	if (n <= 1) {
+		return 0;
+	}
+	long long a = 1, b = 1, c = 1;
+	while (c < n) {
+		a = b;
+		b = c;
+		c = a + b;
+	}
+	return b;
+}
+
+int main() {
+	int n;
+	printf("Nhap so nguyen duong n: ");
+	scanf("%d", &n);
+
+	printf("So Fibonacci lon nhat nhung nho hon %d (khu de quy): %lld\n", n, FibonaccimaxKhuDeQuy(n));
+
+	return 0;
+}
